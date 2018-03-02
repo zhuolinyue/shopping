cd202a0a12258d8ac7e1868e13d589114cfec926
wanglinzai <1026158524@qq.com>
2018-03-02 16:08:28 +0800

    环境设置

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const  path                = require('path');
-const  webpack             = require('webpack')
+const  webpack             = require('webpack');
 const ExtractTextPlugin   = require("extract-text-webpack-plugin");
 const  HtmlWebpackPlugin   = require('html-webpack-plugin');
+let   WEBPACK_ENV          = process.env.WEBPACK_ENV || 'dev' ;
+console.log(WEBPACK_ENV);
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js',
-        publicPath: "/dist/"
+        publicPath: WEBPACK_ENV ==='dev' ? "/dist/" :"//s.jianliwu.com/admin-ve-fe/dist/"
     },
     resolve: {
         alias: {
